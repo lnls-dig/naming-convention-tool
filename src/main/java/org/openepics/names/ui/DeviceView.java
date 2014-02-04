f90e6c8a925d3be8dd23c8bd366f26d24e8157c1
Miha Vitorovič <miha.vitorovic@gmail.com>
2014-02-04 12:48:59 +0100

    DeviceView and ViewFactory can now produce DeviceView objects.

--- src/main/java/org/openepics/names/ui/DeviceView.java
+++ src/main/java/org/openepics/names/ui/DeviceView.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Lists;
 import java.util.List;
 import org.openepics.names.model.DeviceRevision;
 import org.openepics.names.model.UserAccount;
+import org.openepics.names.services.NamingConvention;
 import org.openepics.names.ui.names.NamePartView;
 
 /**
@@ -12,21 +13,28 @@ import org.openepics.names.ui.names.NamePartView;
  */
 public class DeviceView {
 
-    private final DeviceRevision deviceName;
-    private final String conventionName;
+    private final NamingConvention namingConvention;
+    private final ViewFactory viewFactory;
 
-    public DeviceView(DeviceRevision deviceName, String conventionName) {
+    private final DeviceRevision deviceName;
+    private String conventionName;
+
+    public DeviceView(DeviceRevision deviceName, NamingConvention namingConvention, ViewFactory viewFactory) {
         this.deviceName = deviceName;
-        this.conventionName = conventionName;
+        this.namingConvention = namingConvention;
+        this.viewFactory = viewFactory;
     }
 
     public DeviceRevision getDevice() { return deviceName; }
     public Integer getId() { return deviceName.getId(); }
-    public String getConventionName() { return conventionName; }
+    public String getConventionName() {
+        if (conventionName == null) conventionName = namingConvention.getNamingConventionName(this);
+        return conventionName;
+    }
     public String getSectionPath() { return getNamePath(getSection()); }
     public String getDeviceTypePath() { return getNamePath(getDeviceType()); }
-    public NamePartView getSection() { throw new IllegalStateException(); } // { return ViewFactory.getView(deviceName.getSection()); }
-    public NamePartView getDeviceType() { throw new IllegalStateException(); } // { return ViewFactory.getView(deviceName.getDeviceType()); }
+    public NamePartView getSection() { return viewFactory.getView(deviceName.getSection()); }
+    public NamePartView getDeviceType() { return viewFactory.getView(deviceName.getDeviceType()); }
     public String getQualifier() { return deviceName.getQualifier(); }
     public UserAccount getRequestedBy() { return deviceName.getRequestedBy(); }
 
--- src/main/java/org/openepics/names/ui/ViewFactory.java
+++ src/main/java/org/openepics/names/ui/ViewFactory.java
@@ -8,6 +8,8 @@ import org.openepics.names.model.Device;
 import org.openepics.names.model.DeviceRevision;
 import org.openepics.names.model.NamePart;
 import org.openepics.names.model.NamePartRevision;
+import org.openepics.names.services.DeviceService;
+import org.openepics.names.services.NamingConvention;
 import org.openepics.names.services.restricted.RestrictedNamePartService;
 import org.openepics.names.ui.names.NamePartView;
 
@@ -20,6 +22,8 @@ import org.openepics.names.ui.names.NamePartView;
 public class ViewFactory {
 
     @Inject private RestrictedNamePartService namePartService;
+    @Inject private DeviceService deviceService;
+    @Inject private NamingConvention namingConvention;
 
     public NamePartView getView(NamePart namePart) {
         return new NamePartView(namePartService, namePartService.approvedRevision(namePart), namePartService.pendingRevision(namePart));
@@ -34,10 +38,10 @@ public class ViewFactory {
     }
 
     public DeviceView getView(Device device) {
-        throw new IllegalStateException();
+        return new DeviceView(deviceService.currentRevision(device), namingConvention, this);
     }
 
     public DeviceView getView(DeviceRevision deviceRevision) {
-        throw new IllegalStateException();
+        return new DeviceView(deviceRevision, namingConvention, this);
     }
 }
