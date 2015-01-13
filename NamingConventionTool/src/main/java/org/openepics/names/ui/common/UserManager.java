dcb8c9f47e0805cc7b91aabfd7443881242852aa
Karin Rathsman <karin.rathsman@esss.se>
2015-01-13 12:45:32 +0100

    Updated version number (tagged)

--- NamingConventionTool/src/main/java/org/openepics/names/ui/common/UserManager.java
+++ NamingConventionTool/src/main/java/org/openepics/names/ui/common/UserManager.java
@@ -1,6 +1,5 @@
 package org.openepics.names.ui.common;
 
-import org.openepics.names.model.UserAccount;
 import org.openepics.names.services.SessionService;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -17,7 +16,6 @@ public class UserManager {
 
     @Inject private SessionService sessionService;
 
-//    public UserAccount getUser() { return sessionService.user(); }
     public boolean isLoggedIn() { return sessionService.isLoggedIn(); }
     public boolean isEditor() { return sessionService.isEditor(); }
     public boolean isSuperUser() { return sessionService.isSuperUser();} 
